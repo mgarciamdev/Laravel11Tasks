ab2c0e2dfc85775958d4329e0e140b3475b89bc8
Mario Garcia <157403078+mgarciamdev@users.noreply.github.com>
2024-08-27 14:14:33 -0500

    feat: :construction: se agregan migraciones, modelos y se arreglan los seeders

---
+++ database/seeders/TaskSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TaskSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $task = new Task();
+        $task->title = 'Task 1';
+        $task->description = 'Task is a task';
+        $task->user_id = User::find(1)->id;
+        $task->save();
+
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        DB::table('users')->insert([
+            'name' => 'Administrator',
+            'email' => 'admin@example.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'),
+            'remember_token' => Str::random(10),
+        ]);
+    }
+}
